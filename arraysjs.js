9b08066d03e674024e4a5cf0c37c5e9ad325bde4
AshishShankarRenuse <62352258+AshishShankarRenuse@users.noreply.github.com>
2022-04-15 15:35:18 +0530

    Add files via upload

---
+++ arraysjs.js
@@ -0,0 +1,6 @@
+function createarray(){
+var fruits=["Mango","Apple","Banana","Watermeloon","Oranges"];  
+for (i=0;i<fruits.length;i++){ 
+document.write("Fruits["+i+"] :"+fruits[i] + "<br/>");  
+}  
+}
---
+++ eventjs.js
@@ -0,0 +1,13 @@
+function changebgcolor1(){
+document.body.style.backgroundColor = "red";
+}
+
+function changebgcolor2(){
+document.body.style.backgroundColor = "orange";
+}
+
+function welcome(){
+alert("Welcomeee To Eventss!!");
+}
+
+ 
